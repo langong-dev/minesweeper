1439bf11363654baf6a520d5ba28b54fd453025c
langonginc <langonginc@yeah.net>
2020-08-03 10:50:54 +0800

    Push

--- main.cpp
+++ main.cpp
@@ -252,7 +252,7 @@ void InitsialGame ()
 void dead (){
   etime = clock();
   // system ("clear");
-  printf ("\033[31m Boom! You lose!\033[0m\n");
+  printf ("\033[31m Boom! You lose! Used %lf sec.\033[0m\n", double(etime-stime)/CLOCKS_PER_SEC * 1000);
   freopen ("/tmp/tminesweeper-tmp", "w", stdout);
   printf ("User %s lose the game. Used %lf sec.\n", user.user, double(etime-stime)/CLOCKS_PER_SEC * 1000);
   system ("echo $(cat /tmp/tminesweeper-tmp) >> $HOME/.local/share/tminesweeper/log");
